2c69c6e9528ac8293719c6acb2360eddf6d0fd7c
Jonathan Lucero <jlucero@inalambrik.com.ec>
2019-08-29 12:50:47 -0500

    Fix - Relocation of the authorization code in Main.StoryBoard, Definition of Constants of app behavior

---
+++ LocalizadorInalambrik/DatabaseUtilities/ConstantsController.swift
@@ -0,0 +1,23 @@
+//
+//  ConstantsController.swift
+//  LocalizadorInalambrik
+//
+//  Created by Peter Arcentales on 8/29/19.
+//  Copyright © 2019 Inalambrilk. All rights reserved.
+//
+
+import Foundation
+
+class ConstantsController
+{
+    init()
+    {
+    }
+    
+    //To control different behaviour of the app
+    let HTTP_SECURE = false
+    let PRODUCTION_MODE = false
+    let INITIAL_INTERVAL: Double = 10
+    let REPORT_INTERVAL : Double = 30
+    let NUMBER_OF_TRIES = 2
+}
