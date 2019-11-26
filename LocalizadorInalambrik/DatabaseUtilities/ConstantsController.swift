822e26ba54b0ac1e64ecbbe70be6c7278b976ebe
Jonathan Lucero <jonathan.lucero89@inalambrik.com.ec>
2019-11-26 16:52:39 -0500

    Final fixes

--- LocalizadorInalambrik/DatabaseUtilities/ConstantsController.swift
+++ LocalizadorInalambrik/DatabaseUtilities/ConstantsController.swift
@@ -16,13 +16,13 @@ class ConstantsController
     
     //To control different behaviour of the app
     let HTTP_SECURE = false
-    let PRODUCTION_MODE = false
+    let PRODUCTION_MODE = true
     let INITIAL_INTERVAL: Double = 10
     let REPORT_INTERVAL : Double = 120
     let NUMBER_OF_TRIES = 2
     let BEGIN_REPORT_HOUR = 6
     let END_REPORT_HOUR = 22
-    let APP_VERSION = 3.3
+    let APP_VERSION = 3.4
     let NUMBER_OF_MAX_PENDING_REPORTS_TO_SEND = 100
     let NUMBER_OF_MAX_SENT_REPORTS_TO_DELETE = 20
     
